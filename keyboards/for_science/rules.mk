cf06bd574c6de03bd8a3001a1b53b74e69566308
Sono <sono.gomorrha@gmail.com>
2024-04-30 17:21:13 +0200

    changed some things

--- keyboards/for_science/rules.mk
+++ keyboards/for_science/rules.mk
@@ -1,5 +1,7 @@
 SPLIT_KEYBOARD = yes
 COMBO_ENABLE = yes
 UNICODE_COMMON = yes
+CAPS_WORD_ENABLE = yes
+
 
 SRC += features/custom_shift_keys.c //https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
